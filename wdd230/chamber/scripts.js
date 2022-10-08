3cf353131d3146eb45f2cab734c0430298adbf53
eplingk <eplingk@byui.edu>
2022-10-08 13:31:19 -0600

    Fixing Menu

--- wdd230/chamber/scripts.js
+++ wdd230/chamber/scripts.js
@@ -7,31 +7,7 @@ var yyyy = today.getFullYear();
 today = mm + '/' + dd + '/' + yyyy;
 document.write(today);
 
-const menu = document.querySelector(".navLinks");
-const menuItems = document.querySelectorAll(".active");
-const hamburger= document.querySelector(".hamburger");
-const closeIcon= document.querySelector(".closeIcon");
-const menuIcon = document.querySelector(".menuIcon");
 
-function toggleMenu() {
-  if (menu.classList.contains("showMenu")) {
-    menu.classList.remove("showMenu");
-    closeIcon.style.display = "none";
-    menuIcon.style.display = "block";
-  } else {
-    menu.classList.add("showMenu");
-    closeIcon.style.display = "block";
-    menuIcon.style.display = "none";
-  }
-}
-
-hamburger.addEventListener("click", toggleMenu);
-
-menuItems.forEach( 
-  function(menuItem) { 
-    menuItem.addEventListener("click", toggleMenu);
-  }
-)
 
 
 document.querySelector(
