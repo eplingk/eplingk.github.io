82d1cabb86300255769123f67ed6396ff58dbe07
eplingk <eplingk@byui.edu>
2022-10-15 15:55:15 -0600

    Wowthatwasalotofwork :)

--- wdd230/chamber/scripts.js
+++ wdd230/chamber/scripts.js
@@ -1,15 +1,49 @@
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
+//var today = new Date();
+//var dd = String(today.getDate()).padStart(2, '0');
+//var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+//var yyyy = today.getFullYear();
 
 
-today = mm + '/' + dd + '/' + yyyy;
-document.write(today);
+//today = mm + '/' + dd + '/' + yyyy;
+//document.write(today);
 
 
 
 
-document.querySelector(
-	"#lastModified"
-).textContent = `Last Modification: ${document.lastModified}`;
+//document.querySelector(
+//	"#lastModified"
+//).textContent = `Last Modification: ${document.lastModified}`;
+
+    //       var now  = new Date();        // current date/time
+    //       var hrs  = now.getHours();    // 0 to 23
+    //       var mins = now.getMinutes();
+    //       var secs = now.getSeconds();
+        
+        
+    //     var weekday = new Array(7);
+    //   weekday[0]=  "Sunday";
+    //   weekday[1] = "Monday";
+    //   weekday[2] = "Tuesday";
+    //   weekday[3] = "Wednesday";
+    //   weekday[4] = "Thursday";
+    //   weekday[5] = "Friday";
+    //   weekday[6] = "Saturday";
+    //   var day = weekday[now.getDay()];
+      
+      
+          
+    //     document.writeln("</p>");
+    //       if (hrs < 12) {
+    //         document.writeln("<h2>Good Morning!</h2>");
+    //       } else {
+    //         document.writeln("<h2>Good Afternoon!</h2>");
+    //       }
+    //     if (day == "Monday") {
+    //         document.writeln("<h2>🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.</h2>");
+    //       } 
+    //     else if (day == "Saturday"){
+    //         document.writeln("<h2>🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.</h2>");
+    //       }
+    //      else {
+    //         document.writeln("<h2>Did you miss our meet and greet? No worries! There are always chances to meet your fellow commerce members.</h2>");
+    //       }
