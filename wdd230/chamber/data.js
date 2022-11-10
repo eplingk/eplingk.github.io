015f71f8948a2a15ca932176e0d6732aa136c232
eplingk <eplingk@byui.edu>
2022-11-10 14:52:35 -0700

    linking json file

--- wdd230/chamber/data.js
+++ wdd230/chamber/data.js
@@ -1,4 +1,4 @@
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const requestURL = 'https://eplingk.github.io/wdd230/chamber/data.json';
 const cards = document.querySelector('.cards');
 
 fetch(requestURL)  //feed the required arguments, the URL 
