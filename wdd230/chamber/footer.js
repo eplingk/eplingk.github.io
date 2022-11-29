9e6036b733741b8a72c5e8f2d0153ea83e73f4b0
eplingk <eplingk@byui.edu>
2022-11-29 13:27:43 -0700

    done?

--- wdd230/chamber/footer.js
+++ wdd230/chamber/footer.js
@@ -1,1 +1,1 @@
-document.getElementById("lastModified").innerHTML = `©2022 IF Chamber of Commerce | Katie Epling | WDD 230 Project |Weather info provided by: OpenWeather API| Last Modification: ${document.lastModified}`;
+document.getElementById("lastModified").innerHTML = `©2022 IF Chamber of Commerce | Katie Epling | WDD 230 Project | Last Modification: ${document.lastModified}`;
--- wdd230/chamber/scripts.js
+++ wdd230/chamber/scripts.js
@@ -1,17 +1,7 @@
 
-// // select the elements to manipulate (output to)
-// const datefield = document.querySelector(".date");
-// const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+// hidden field for join form
+var day = d.getDate();
+var month = d.getMonth() + 1; 
+var year = d.getFullYear();
 
-// // derive the current date using a date object
-// const today = new Date();
-// const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-// 	today
-// );
-// const fulldateUK = new Intl.DateTimeFormat("en-UK", {
-// 	dateStyle: "full"
-// }).format(today);
-// // long, medium, short options ... try them
-
-// datefield.innerHTML = `<em>${fulldate}</em>`;
-// datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+document.getElementById("dayAndTime").innerHTML.valueOf = day + "/" + month + "/" + year;
