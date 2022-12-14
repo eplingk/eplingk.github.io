832e8813d4d12559a0cea7f3d7197ec1d727b281
eplingk <eplingk@byui.edu>
2022-12-14 12:44:44 -0700

    quick save

--- wdd230/bountiful/inputdisplaytwo.js
+++
@@ -1,7 +0,0 @@
-function showDropInfo() {
-    var sT = dropForm.dropSelect;
-    var selection = document.getElementById('selection');
-    
-    selection.innerHTML = ('Selected fruit: ' + sT[sT.selectedIndex].text);
-  
- }
